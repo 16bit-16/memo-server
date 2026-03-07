2be72a2385dbf404f80a570c63d2080d95389e72
16bit-16 <lhy93380@gmail.com>
2026-03-07 12:03:50 +0900

    add: cors localhost 추가

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.use(express.static('public'));
 app.use(cors({
   origin: [
     'https://apah.site',
-    'https://www.apah.site'
+    'https://www.apah.site',
+    'http://localhost:3000',
+    'http://localhost:5500',
+    'http://127.0.0.1:5500'
   ],
   credentials: true
 }));
